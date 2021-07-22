6a1bcfe4378dae72d1d188c76d189cd3d25e3eaa
Ashish Shakya <ashishakya@outlook.com>
2021-07-22 17:59:47 +0545

    santum installation
    [x] sanctum setup

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\Employee\EmployeeController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +18,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get(
+    '/user',
+    function (Request $request) {
+        return $request->user();
+    }
+);
+Route::get(
+    'users',
+    function () {
+        return User::all();
+    }
+)->middleware('auth:sanctum');
 
+Route::middleware('auth:sanctum')->get(
+    'me',
+    function (Request $request) {
+        return $request->user();
+    }
+);
+
+Route::post(
+    'login',
+    function (Request $request) {
+        $request->validate(
+            [
+                'email'    => 'required|email',
+                'password' => 'required',
+            ]
+        );
+
+        $user = User::where('email', $request->email)->first();
+
+        if ( !$user || !Hash::check($request->password, $user->password) ) {
+            throw ValidationException::withMessages(
+                [
+                    'email' => ['The provided credentials are incorrect.'],
+                ]
+            );
+        }
+
+        return $user->createToken($request->email)->plainTextToken;
+    }
+);
 Route::resource("employees", EmployeeController::class)->only(["store", "index", "show"]);
